fba3cc04b215e684b69800d900b2c0a74614f612
kyraantonio <kyra.antonio@gmail.com>
2018-04-12 08:12:28 +0800

    Card Columns
    Co-Authored-By: GISSI-Jazmin <gissi-jazmin@users.noreply.github.com>

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -3,8 +3,8 @@
 <div class="main-panel">
     <div class="content">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="card-columns">
+                <!-- <div class="col-lg-4 col-md-6 col-sm-6"> -->
                     <div class="card">
                         <div class="card-header card-header-warning">
                             <h4 class="card-title"><a href="{{url('company/' . $company_id . '/projects')}}">Projects</a></h4>
@@ -23,8 +23,8 @@
                             </ul>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <!-- </div> -->
+                <!-- <div class="col-lg-4 col-md-6 col-sm-6"> -->
                     <div class="card">
                         <div class="card-header card-header-danger">
                             <h4 class="card-title"><a href="{{url('company/' . $company_id . '/jobs')}}">Jobs</a></h4>
@@ -43,8 +43,8 @@
                             </ul>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <!-- </div>
+                <div class="col-lg-4 col-md-6 col-sm-6"> -->
                     <div class="card">
                         <div class="card-header card-header-success">
                             <h4 class="card-title"><a href="{{url('quizPerCompany/' . $company_id)}}">Tests</a></h4>
@@ -63,97 +63,98 @@
                             </ul>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="card">
-                        <div class="card-header card-header-info">
-                            <h4 class="card-title"><a href="{{url('employees/' . $company_id)}}">Employees</a></h4>
-                        </div>
-                        <div class="portlet-content">
-                            <ul class='list-group'>
-                               @foreach($employees as $employee)
-                                <li class='list-group-item'>
-                                    @if(strlen($employee->user->name) > 23)
-                                    <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$employee->user->name}}" href="{{url('user/'.$employee->user_id.'/company/'.$employee->company_id)}}">{{$employee->user->name}}</a>
-                                    @else
-                                    <a target="_blank" href="{{url('user/'.$employee->user_id.'/company/'.$employee->company_id)}}">{{$employee->user->name}}</a>
-                                    @endif
 
-                                </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="card">
-                        <div class="card-header card-header-primary">
-                            <h4 class="card-title"><a href="{{url('companyLinks/' . $company_id)}}">Links</a></h4>
-                        </div>
-                        <div class="portlet-content">
-                            <ul class='list-group'>
-                               @foreach($links as $link)
-                                <li class='list-group-item'>
-                                    {{--*/ $parse_url = parse_url($link->url) /*--}}
-                                    {{--*/ $url = empty($parse_url['scheme']) ? 'http://' . $link->url :  $link->url /*--}}
-                                    @if(strlen($link->title) > 23)
-                                    <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$link->title}}" href="{{$url}}">{{$link->title}}</a>
-                                    @else
-                                    <a target="_blank" href="{{$url}}">{{$link->title}}</a>
-                                    @endif
-                                </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                    <!-- </div> -->
+                    <!-- <div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-6"> -->
+                            <div class="card">
+                                <div class="card-header card-header-info">
+                                    <h4 class="card-title"><a href="{{url('employees/' . $company_id)}}">Employees</a></h4>
+                                </div>
+                                <div class="portlet-content">
+                                    <ul class='list-group'>
+                                       @foreach($employees as $employee)
+                                        <li class='list-group-item'>
+                                            @if(strlen($employee->user->name) > 23)
+                                            <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$employee->user->name}}" href="{{url('user/'.$employee->user_id.'/company/'.$employee->company_id)}}">{{$employee->user->name}}</a>
+                                            @else
+                                            <a target="_blank" href="{{url('user/'.$employee->user_id.'/company/'.$employee->company_id)}}">{{$employee->user->name}}</a>
+                                            @endif
 
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="card">
-                        <div class="card-header card-header-rose">
-                            <h4 class="card-title">Applicants</h4>
-                        </div>
-                        <div class="portlet-content">
-                            <ul class='list-group'>
-                               @foreach($applicants as $applicant)
-                                <li class='list-group-item'>
-                                    @if(strlen($applicant->name) > 23)
-                                    <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$applicant->name}}" href="{{url('applicant/'.$applicant->id)}}">{{$applicant->name}}</a>
-                                    @else
-                                    <a target="_blank" href="{{url('applicant/'.$applicant->id)}}">{{$applicant->name}}</a>
-                                    @endif
-                                </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        <!-- </div> -->
+                        <!-- <div class="col-lg-4 col-md-6 col-sm-6"> -->
+                            <div class="card">
+                                <div class="card-header card-header-primary">
+                                    <h4 class="card-title"><a href="{{url('companyLinks/' . $company_id)}}">Links</a></h4>
+                                </div>
+                                <div class="portlet-content">
+                                    <ul class='list-group'>
+                                       @foreach($links as $link)
+                                        <li class='list-group-item'>
+                                            {{--*/ $parse_url = parse_url($link->url) /*--}}
+                                            {{--*/ $url = empty($parse_url['scheme']) ? 'http://' . $link->url :  $link->url /*--}}
+                                            @if(strlen($link->title) > 23)
+                                            <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$link->title}}" href="{{$url}}">{{$link->title}}</a>
+                                            @else
+                                            <a target="_blank" href="{{$url}}">{{$link->title}}</a>
+                                            @endif
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        <!-- </div>
 
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card">
-                    <div class="card-header card-header-danger">
-                        <h4 class="card-title">Briefcase Items</h4>
-                    </div>
-                    <div class="portlet-content">
-                        <ul class='list-group'>
-                           @foreach($items as $item)
-                            <li class='list-group-item'>
-                                @if(strlen($item->checklist_header) > 23)
-                                <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$item->checklist_header}}" href="{{url('taskitem/'.$item->id)}}">{{$item->checklist_header}}</a>
-                                @else
-                                <a target="_blank" href="{{url('taskitem/'.$item->id)}}">{{$item->checklist_header}}</a>
-                                @endif
-                            </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6"> -->
+                            <div class="card">
+                                <div class="card-header card-header-rose">
+                                    <h4 class="card-title">Applicants</h4>
+                                </div>
+                                <div class="portlet-content">
+                                    <ul class='list-group'>
+                                       @foreach($applicants as $applicant)
+                                        <li class='list-group-item'>
+                                            @if(strlen($applicant->name) > 23)
+                                            <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$applicant->name}}" href="{{url('applicant/'.$applicant->id)}}">{{$applicant->name}}</a>
+                                            @else
+                                            <a target="_blank" href="{{url('applicant/'.$applicant->id)}}">{{$applicant->name}}</a>
+                                            @endif
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="card">
+                                <div class="card-header card-header-danger">
+                                    <h4 class="card-title">Briefcase Items</h4>
+                                </div>
+                                <div class="portlet-content">
+                                    <ul class='list-group'>
+                                       @foreach($items as $item)
+                                        <li class='list-group-item'>
+                                            @if(strlen($item->checklist_header) > 23)
+                                            <a target="_blank" data-toggle="tooltip" data-placement="top" title="{{$item->checklist_header}}" href="{{url('taskitem/'.$item->id)}}">{{$item->checklist_header}}</a>
+                                            @else
+                                            <a target="_blank" href="{{url('taskitem/'.$item->id)}}">{{$item->checklist_header}}</a>
+                                            @endif
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                            @include('common.note',['note' => $note, 'belongs_to' => 'company', 'unique_id' => $company_id])
+                        </div>
+                    <!-- </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6"> -->  
+                    <!-- </div> -->
+
                 </div>
-                @include('common.note',['note' => $note, 'belongs_to' => 'company', 'unique_id' => $company_id])
-            </div>
 
         </div>
 
